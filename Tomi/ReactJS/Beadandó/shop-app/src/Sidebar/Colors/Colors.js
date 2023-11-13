feec946b27cf3996dbf72bf9ce3b79d704f7e578
HeyItsMedard <62563792+HeyItsMedard@users.noreply.github.com>
2023-11-13 18:59:54 +0100

    proceeding with shop app react

--- Tomi/ReactJS/Beadandó/shop-app/src/Sidebar/Colors/Colors.js
+++ Tomi/ReactJS/Beadandó/shop-app/src/Sidebar/Colors/Colors.js
@@ -1,7 +1,38 @@
-import './Colors.css'
+import "./Colors.css";
+import Input from "../../components/Input";
 
-function Colors() {
-    return<div>Colors</div>;
-  }
-  
+const Colors = ({ handleChange }) => {
+  return (
+    <>
+      <div>
+        <h2 className="sidebar-title color-title">Colors</h2>
+        <label className="sidebar-label-container color-title">
+          <input onChange={handleChange} type="radio" value="" name="test1" />
+          <span className="checkmark all"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test1" />
+          <span className="checkmark"></span>Black
+        </label>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test1" />
+          <span className="checkmark"></span>White
+        </label>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test1" />
+          <span className="checkmark"></span>Blue
+        </label>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test1" />
+          <span className="checkmark"></span>Red
+        </label>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test1" />
+          <span className="checkmark"></span>Green
+        </label>
+      </div>
+    </>
+  );
+};
+
 export default Colors;
