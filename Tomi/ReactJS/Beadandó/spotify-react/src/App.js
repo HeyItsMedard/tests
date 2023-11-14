d59ca2f17a604e1acd976e91472f098f48b25d75
HeyItsMedard <62563792+HeyItsMedard@users.noreply.github.com>
2023-11-14 20:32:29 +0100

    working artist-album search (no fav)

--- Tomi/ReactJS/Beadandó/spotify-react/src/App.js
+++ Tomi/ReactJS/Beadandó/spotify-react/src/App.js
@@ -1,82 +1,99 @@
 import {useEffect, useState} from "react";
 import './App.css';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
+
+const CLIENT_ID = "ecba8775c2de416b9424c53286d9868a"
+const CLIENT_SECRET = "0f765175790b438db56a6441de66ffe9"
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
 
 function App() {
-    const CLIENT_ID = "ecba8775c2de416b9424c53286d9868a"
-    const REDIRECT_URI = "http://localhost:3000"
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-    const RESPONSE_TYPE = "token"
-
-    const [token, setToken] = useState("")
-    const [searchKey, setSearchKey] = useState("")
-    const [artists, setArtists] = useState([])
+    const [searchInput, setSearchInput] = useState("")
+    const [accessToken, setAccessToken] = useState("")
+    const [albums, setAlbums] = useState("")
+    // const [artists, setArtists] = useState([])
 
     useEffect(() => {
-        const hash = window.location.hash
-        let token = window.localStorage.getItem("token")
-
-        if (!token && hash) {
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-            window.location.hash = ""
-            window.localStorage.setItem("token", token)
+        var authParameters = {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'grant_type=client_credentials&client_id=' +CLIENT_ID + '&client_secret=' + CLIENT_SECRET
         }
-
-        setToken(token)
-
+        fetch(`https://accounts.spotify.com/api/token`, authParameters)
+            .then(result => result.json())
+            .then(data => setAccessToken(data.access_token))
     }, [])
 
-    const logout = () => {
-        setToken("")
-        window.localStorage.removeItem("token")
-    }
-
-    const searchArtists = async (e) => {
-        e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+    async function search() {
+        console.log("Search for " +searchInput);
+        var searchParameters = {
+            method: 'GET',
             headers: {
-                Authorization: `Bearer ${token}`,
-            },
-            params: {
-                q: searchKey,
-                type: "artist",
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+ accessToken
             }
-        })
-
-        setArtists(data.artists.items)
+        }
+        var artistID = await fetch('https://api.spotify.com/v1/search?q='+searchInput+'&type=artist', searchParameters)
+            .then(response => response.json())
+            .then(data => {return data.artists.items[0].id})
+        console.log('Artist ID: ' +artistID)
+        var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID+'/albums'+ '?include_groups=album&market=US&limit=50', searchParameters)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setAlbums(data.items)})
     }
+    // const searchArtists = async (e) => {
+    //     e.preventDefault()
+    //     const {data} = await axios.get("https://api.spotify.com/v1/search", {
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //         },
+    //         params: {
+    //             q: searchKey,
+    //             type: "artist",
+    //         }
+    //     })
 
-    const renderArtists = () => {
-        return artists.map(artist => (
-            <div key={artist.id}>
-                {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-                {artist.name}
-            </div>
-        ))
-    }
+    //     setArtists(data.artists.items)
+    // }
 
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Spotify React</h1>
-                {!token ?
-                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
-                        to Spotify</a>
-                    : <button onClick={logout}>Logout</button>}
-
-                {token ?
-                    <form onSubmit={searchArtists}>
-                        <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-                        <button type={"submit"}>Search</button>
-                    </form>
-
-                    : <h2>Please login</h2>
-                }
-
-                {renderArtists()}
-
-            </header>
+            <Container>
+                <InputGroup className="mb-3" size="lg">
+                    <FormControl 
+                        placeholder="Search for Artist"
+                        type="input"
+                        onKeyPress={event => {
+                            if (event.key == "Enter"){
+                                search();
+                            }
+                        }}
+                        onChange={event => setSearchInput(event.target.value)}
+                    />
+                    <Button onClick={search}>
+                        search
+                    </Button>
+                </InputGroup>
+            </Container>
+            <Container>
+                <Row className="mx-2 row row-cols-4"></Row>
+                {albums.map((album, i)=>{
+                    return (<Card>
+                        <Card.Img src = {album.images[0].url}/>
+                        <Card.Body>
+                            <Card.Title>{album.name}</Card.Title>
+                        </Card.Body>  
+                    </Card>)
+                })}
+                
+            </Container>
         </div>
     );
 }
