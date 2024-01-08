b31c91dd338d2e58b179e3de99697369621cfbbb
HeyItsMedard <62563792+HeyItsMedard@users.noreply.github.com>
2024-01-08 18:24:34 +0100

    menü ok

--- Prog2Beadandó/app.py
+++ Prog2Beadandó/app.py
@@ -1,31 +1,23 @@
+# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-#THIS SHOULD BE THE MAIN PAGE
-@app.route('/menu')
-def menu():
-    pass
-
-@app.route('/fetch')
-def fetch():
-    pass #onpress, update db (drop previous)
+    return render_template('base.html')
 
 @app.route('/game')
 def game():
-    pass
+    return render_template('game.html')
 
-@app.route('/game_over')
-def game_over():
-    pass
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
 
-@app.route('/statistics')
-def statistics():
-    pass
+@app.route('/fetch')
+def fetch():
+    return render_template('fetch.html') # drop table? yes/no; append sqlalchemy db
 
 if __name__ == "__main__":
     app.run(debug=True)
